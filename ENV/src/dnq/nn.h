7f9b7ccc3dc1bb6760b0ebea24da78e655429317
yaolubrain <yaolubrain@gmail.com>
2014-03-15 15:19:57 +0200

    Update nn.h

--- ENV/src/dnq/nn.h
+++ ENV/src/dnq/nn.h
@@ -16,23 +16,27 @@ class NN {
   NN(NNParameter* nn_para) {
     im_size_ = nn_para->im_size_;
     data_batch_num_ = nn_para->data_batch_num_;
-    data_batch_size_ = nn_para->data_batch_size_;
+    data_batch_size_ = nn_para->data_batch_size_;   
     sample_size_ = data_batch_num_ * data_batch_size_;
-    channel_num_ = nn_para->channel_num_;
-    class_num_ = nn_para->class_num_;
+    channel_num_ = nn_para->channel_num_; 
+    class_num_ = nn_para->class_num_;     
 
-    epoch_num_ = nn_para->epoch_num_;
-    mb_size_ = nn_para->mb_size_;
+    epoch_num_ = nn_para->epoch_num_;  
+    mb_size_ = nn_para->mb_size_;  
     learn_rate_ = nn_para->learn_rate_;
-    momentum_ = nn_para->momentum_;
-    nn_para_ = nn_para;
+    momentum_ = nn_para->momentum_;        
+    file_names_ = nn_para->file_names_;  
+
+    nn_para_ = nn_para;    
   };
 
   ~NN() {
-
+    delete[] data_im_;
+    delete[] labels_;
   };
 
   void Init();
+  void LoadData();    
   void RandPerm(int *randperm, int size);
   void Train();
   void Test();
@@ -48,11 +52,13 @@ class NN {
   int epoch_num_;  
   int mb_size_;  
   double learn_rate_;
-  double momentum_;
+  double momentum_;    
+  FileNames file_names_;
 
   NNParameter* nn_para_;
 
-
+  double** data_im_;
+  uInt8* labels_;
 
   std::vector<Layer*> layers_;
 
